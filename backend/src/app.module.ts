fd89fa251d9a089679c719da11dd72bc56cb3d9d
Ekaterina Kryuchkova <krekser37@yandex.ru>
2023-03-27 10:18:02 +0300

    feat: add .env< update dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,17 +12,21 @@ import { User } from './users/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import configurations from './utils/configurations';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const { POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_USER, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_PGDATA } = process.env;
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [configurations] }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
+      type: process.env.POSTGRES_PGDATA as 'postgres' || 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
       port: 5432,
-      username: 'student',
-      password: 'student',
-      database: 'kupipodariday',
+      username: process.env.POSTGRES_USER || 'student',
+      password: process.env.POSTGRES_PASSWORD || 'student',
+      database: process.env.POSTGRES_DB || 'kupipodariday',
       entities: [User, Wishlist, Wish, Offer],
       synchronize: true,
     }),
